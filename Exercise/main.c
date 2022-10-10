9139ea8427598d5161d8bdfd21103be93d06e53e
vanquyen1021 <vanquyen8499@gmail.com>
2022-10-10 22:49:07 +0700

    GetDataFromFile()

--- Exercise/main.c
+++ Exercise/main.c
@@ -3,6 +3,7 @@
 
 void Create(char *pFileName);
 void AddNewStudent(char *pFileName, double x, double y, double z);
+double GetDataFromFile(char *pFileName, int position);
 
 int main(int argc, char *argv[])
 {
@@ -10,8 +11,9 @@ int main(int argc, char *argv[])
     double num = strtod("123.0", NULL);
     printf("%0.2f :\n", num);
 
-    Create("file1.txt");
-    AddNewStudent("file1.txt", 4.5, 10, 7.8);
+    // Create("file1.txt");
+    // AddNewStudent("file1.txt", 4.5, 10, 7.8);
+    printf("Get Data: %0.2f\n",GetDataFromFile("file1.txt",5));
 }
 
 void Create(char *pFileName)
@@ -58,8 +60,50 @@ void AddNewStudent(char *pFileName, double x, double y, double z)
 
         fprintf(LpFile,"%d \t\t %0.2f \t\t %0.2f \t\t\t %0.2f \t\t\t %0.2f\n", Id, x, y, z, Avr);
 
-        printf("Recorded successfully!\n");
+        printf("Record is successfully!\n");
 
         fclose(LpFile);
     }
+}
+
+double GetDataFromFile(char *pFileName, int position)
+{
+    FILE *LpFile;
+
+    LpFile = fopen(pFileName, "r");
+    double a,b,c,d,e;
+
+    char x[6], y[6], z[6], avr[6];
+    int Id;
+
+    fscanf(LpFile,"%d %s %s %s %s", &Id, x, y, z, avr);
+    
+    switch (position)
+    {
+    case 2:
+        return strtod(x, NULL);
+        break;
+    case 3:
+        return strtod(y, NULL);
+        break;
+    case 4:
+        return strtod(y, NULL);
+        break;
+    case 5:
+        return strtod(avr, NULL);
+        break;
+    default:
+        return Id;
+        break;
+    }
+
+    // printf("Begin\n");
+    // printf("1: %d\n", Id);
+    // printf("2: %s\n",x);
+    // printf("3: %s\n",y);
+    // printf("4: %s\n",z);
+    // printf("5: %s\n",avr);
+    // printf("Done\n");
+
+    fclose(LpFile);
 }
